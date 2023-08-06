7be1b1abc9691a892f166e332db0b04097e7d12f
Ninelven <141583034+Ninelven@users.noreply.github.com>
2023-08-07 01:49:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+function sendEmail() {
+    window.location.href = "mailto:anshumantiwari971@gmail.com";
+}
+
+// ASCII  ART
+
+const visualText = `
+███████████████████████████
+███████▀▀▀░░░░░░░▀▀▀███████
+████▀░░░░░░░░░░░░░░░░░▀████
+███│░░░░░░░░░░░░░░░░░░░│███
+██▌│░░░░░░░░░░░░░░░░░░░│▐██
+██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+██▌░│██████▌░░░▐██████│░▐██
+███░│▐███▀▀░░▄░░▀▀███▌│░███
+██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+████▄─┘██▌░░░░░░░▐██└─▄████
+█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+███████▄░░░░░░░░░░░▄███████
+██████████▄▄▄▄▄▄▄██████████
+███████████████████████████
+`;
+
+const visualChunks = visualText.match(/.{1,80}/g); 
+
+function displayVisualChunks(index) {
+if (index >= visualChunks.length) {
+return;
+}
+
+console.warn(visualChunks[index]);
+
+setTimeout(() => {
+displayVisualChunks(index + 1);
+}, 1000); 
+}
+
+displayVisualChunks(0);
+
+console.log("Stay Away Hacker :)");
